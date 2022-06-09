b4487e1ab1a032217d06b1e94a84ff3850a7505a
Debby95 <debbyjacksonstar@hotmail.it>
2022-06-10 01:53:25 +0200

    + testo e btn

--- bonus/script.js
+++ bonus/script.js
@@ -1,12 +1,17 @@
 const root = new Vue({
-    el:'#root',
+    el:'#app',
     data: {
         h1Text: "Counter Strike: Global Offensive",
         imgCS: "img/fb_image.png",
-        h2Text: "PGL Major Stockholm 2021",
         logoMajor: "img/Pgl_major_stockholm_2021_logo.png",
         majorLocation: "Avicii Arena, Stockholm, Sweden",
         majorDates: "October 26-November 7, 2021",
-        majorInfo: "Twenty-four teams qualified via regional major rankings. It featured a US$2,000,000 prize pool, a rise from the $1,000,000 of previous Majors due to the absence of offline competition amid the COVID-19 pandemic. It was the second Major hosted by the Romanian organization PGL, after PGL Major: Kraków 2017.  Stockholm 2021 was the first Major after a break caused by the COVID-19 pandemic following the StarLadder Major: Berlin 2019. The Major was won by Natus Vincere, who went undefeated without losing a single map throughout the tournament. ",
+        isVisible: false,
+    },
+    methods: {
+        visiblePost(){
+            this.isVisible = !this.isVisible;
+        },
+        
     },
 });
